5ca774524e1137a7a11b1243e2bb4322dc424f14
Sam <23609502+sabmos@users.noreply.github.com>
2018-08-03 10:32:09 +0800

    Finished Project
    Added animations, scenes, scripts, controls, streaming features, credits, photos and particles

---
+++ Storytellers Revenge/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+
+    public Animator animator;
+
+    private int levelToLoad;
+
+	// Update is called once per frame
+	void Update () {
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            GoToScene("TitleScreen");
+        }
+        */
+	}
+
+    public void GoToMainMenu() {
+        levelToLoad = 0;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToRainbow()
+    {
+        levelToLoad = 1;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToFerry() {
+        levelToLoad = 2;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToCliffJump()
+    {
+        levelToLoad = 3;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToFar()
+    {
+        levelToLoad = 4;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToQatar()
+    {
+        levelToLoad = 5;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void GoToCredits()
+    {
+        levelToLoad = 6;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void LoadLevel ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Storytellers Revenge/Assets/Scripts/VideoControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoControl : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject pauseMenu;
+    public UnityEngine.UI.Button nextButton;
+
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+    private double minLength = 20;
+
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+
+    void Start()
+    {
+        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+
+
+        if (videoPlayer.clip != null)
+        {
+            videoPlayer.EnableAudioTrack(0, true);
+            videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        }
+
+    }
+
+    //Check if input keys have been pressed and call methods accordingly.
+    void Update()
+    {
+        //Play or pause the video.
+        if (Input.GetKeyDown("space"))
+        {
+            if (videoPlayer.isPlaying)
+                videoPlayer.Pause();
+            else
+                videoPlayer.Play();
+            audioSource.Play();
+        }
+
+        if (!videoPlayer.isPlaying && !mainMenu.activeSelf) {
+            mainMenu.SetActive(true);
+            pauseMenu.SetActive(false);
+        } else if (videoPlayer.isPlaying && mainMenu.activeSelf) {
+            mainMenu.SetActive(false);
+            pauseMenu.SetActive(true);
+        }
+
+        //  Change to next video at the end of the video
+
+        if (videoPlayer.time >= minLength &&
+            videoPlayer.frame >= (double)videoPlayer.frameCount - 100)
+        {
+            nextButton.onClick.Invoke();
+        }
+        Debug.Log(videoPlayer.frame.ToString() + " " + videoPlayer.frameCount.ToString());
+    }
+
+    public void Pause() {
+        if (videoPlayer.isPlaying) 
+            videoPlayer.Pause();
+    }
+
+    public void Play() {
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+
+    public void Restart() {
+        videoPlayer.Stop();
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
